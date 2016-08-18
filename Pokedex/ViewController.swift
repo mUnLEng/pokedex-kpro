f22fe9d3a5627ce31a453747d56ac0b32651c489
mUnLEng <thesggamerstsg@gmail.com>
2016-08-18 22:09:04 +0800

    added project

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Samson Pang on 18/8/16.
+//  Copyright © 2016 KProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
